958b06fdabfa78096874369d0cce717b70add39e
WasiqB <wasbhamla2005@gmail.com>
2023-09-25 23:20:23 +0530

    Initial commit

---
+++ src/main/java/com/github/wasoqb/page/SignUpPage.java
@@ -0,0 +1,50 @@
+package com.github.wasoqb.page;
+
+import static io.appium.java_client.AppiumBy.accessibilityId;
+import static io.appium.java_client.AppiumBy.androidUIAutomator;
+import static java.time.Duration.ofSeconds;
+import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
+
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class SignUpPage {
+    private final By            confirmPassword = accessibilityId ("input-repeat-password");
+    private final By            email           = accessibilityId ("input-email");
+    private final By            loginTab        = accessibilityId ("Login");
+    private final By            message         = androidUIAutomator (
+        "new UiSelector().resourceId(\"android:id/message\")");
+    private final By            okButton        = androidUIAutomator (
+        "new UiSelector().resourceId(\"android:id/button1\")");
+    private final By            password        = accessibilityId ("input-password");
+    private final By            signUpButton    = accessibilityId ("button-SIGN UP");
+    private final By            signUpTab       = accessibilityId ("button-sign-up-container");
+    private final WebDriverWait wait;
+
+    public SignUpPage (final AndroidDriver driver) {
+        this.wait = new WebDriverWait (driver, ofSeconds (10));
+    }
+
+    public String signUp (final String userName, final String pass) {
+        this.wait.until (elementToBeClickable (this.loginTab))
+            .click ();
+        this.wait.until (elementToBeClickable (this.signUpTab))
+            .click ();
+        this.wait.until (elementToBeClickable (this.email))
+            .sendKeys (userName);
+        this.wait.until (elementToBeClickable (this.password))
+            .sendKeys (pass);
+        this.wait.until (elementToBeClickable (this.confirmPassword))
+            .sendKeys (pass);
+        this.wait.until (elementToBeClickable (this.signUpButton))
+            .click ();
+
+        final var messageText = this.wait.until (visibilityOfElementLocated (this.message))
+            .getText ();
+        this.wait.until (elementToBeClickable (this.okButton))
+            .click ();
+        return messageText;
+    }
+}
---
+++ src/test/java/com/github/wasoqb/WdioDemoTest.java
@@ -0,0 +1,49 @@
+package com.github.wasoqb;
+
+import static java.lang.System.getProperty;
+import static java.time.Duration.ofSeconds;
+import static org.testng.Assert.assertEquals;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Path;
+
+import com.github.wasoqb.page.SignUpPage;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class WdioDemoTest {
+    private AndroidDriver driver;
+
+    @BeforeClass
+    public void setupClass () throws MalformedURLException {
+        final var options = new UiAutomator2Options ();
+        options.setApp (Path.of (getProperty ("user.dir"), "src/test/resources/wdio-demo.apk")
+                .toString ())
+            .setDeviceName ("Pixel 7 Pro")
+            .setAvd ("Pixel_7_Pro")
+            .setAppWaitActivity ("com.wdiodemoapp.MainActivity")
+            .setCapability ("appium:settings[ignoreUnimportantViews]", true);
+
+        this.driver = new AndroidDriver (new URL ("http://localhost:4723/"), options);
+        this.driver.manage ()
+            .timeouts ()
+            .implicitlyWait (ofSeconds (5));
+    }
+
+    @AfterClass
+    public void teardownClass () {
+        this.driver.quit ();
+    }
+
+    @Test
+    public void testSignUp () {
+        final var signUpPage = new SignUpPage (this.driver);
+        final var actualMessage = signUpPage.signUp ("admin@gmail.com", "Admin@1234");
+
+        assertEquals (actualMessage, "You successfully signed up!");
+    }
+}
